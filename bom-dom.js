e29364a84652df82fa9fe2e1deb236354897cc91
Jerinislam05 <jerinislam05@gmail.com>
2024-01-25 15:05:35 +0000

    browser explorer

---
+++ bom-dom.js
@@ -0,0 +1,8 @@
+/* 
+BOM vs DOM
+1. BOM-Browser Object Model and DOM-Document Object Model.
+2. BOM- used to access and manipulation of the browser window on the other side DOM- used to manipulate the HTML document
+3. Each browser has its own standards for implementing BOM and DOM has a set standard of rules to be used accross documents
+4. BOM consists of the objects navigator, history, screen, location on the other side DOM represents the contents of the web page.
+5.BOM is superset of DOM and DOM is subset of BOM.
+*/
