8eedb79ca97085cbb09afa22516ea79c7fb21d3e
Jerinislam05 <jerinislam05@gmail.com>
2024-01-26 11:50:59 +0000

    five debugging techniques

---
+++ debug.js
@@ -0,0 +1,15 @@
+/* Some Debugging Technics 
+
+1. The console.log method
+2. Setting the breakpoint
+3. Using the debugger keyword
+4. Backtracking 
+*/
+
+/* Different types  of error
+1. Syntax error
+2. Type error
+3. Reference error
+4. Internal error
+5. Range error
+*/
---
+++ debug2.js
@@ -0,0 +1,8 @@
+/* 
+Five techniques to master in debugging:
+1. Backward(line khuje the nottom to up jete hobe)
+2. Forward tracking(soja dike jete hobe)
+3. Elimination/commenting technique
+4. Breakpoint
+5. Brute force
+*/
